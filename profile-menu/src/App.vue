98fbce5e536b5676c5aa5e46ff35ca2ad1226fc3
chicobami <58246272+chicobami@users.noreply.github.com>
2025-03-14 19:30:58 +0900

    setting html

--- profile-menu/src/App.vue
+++ profile-menu/src/App.vue
@@ -1,8 +1,56 @@
 <template>
+  <div class="container">
+    <header>
+      <h1>Profile</h1>
+      <!-- menu -->
+      <nav>
+        <ul>
+        <li>about</li>
+        <li>bicycle</li>
+        </ul>
+      </nav>
+    </header>
 
-<div>
-  <span>Hello world!</span>
-</div>
+    <div>
+      <img src="./assets/profile1.svg" alt="top image">
+    </div>
+    <div>
+      <h1>About</h1>
+    </div>
+    <div>
+      <img src=".assets/profile2.svg" alt="profile image">
+    </div>
+    <div>
+      <span>KAKERU MIYAICHI</span>
+      <p>テキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>テキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>テキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div>
+      <h1>Bicycle</h1>
+    </div>
+    <div>
+      <div>
+        <img src="./assets/bicycle1.svg" alt="bicycle1 image">
+        <span>タイトル</span>
+        <p>テキストテキストテキスト</p>
+      </div>
+      <div>
+        <img src="./assets/bicycle2.svg" alt="bicycle2 image">
+        <span>タイトル</span>
+        <p>テキストテキストテキスト</p>
+      </div>
+      <div>
+        <img src="./assets/bicycle3.svg" alt="bicycle3 image">
+        <span>タイトル</span>
+        <p>テキストテキストテキスト</p>
+      </div>
+    </div>
+
+    <footer>
+      <p>©︎2020 Profile</p>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss">
